16a5090b958789b2069ab391378d0cb11ff617d9
Rémi Alvergnat <toilal.dev@gmail.com>
2018-01-11 21:22:22 +0100

    Test # import TSLint/TypeCheck

---
+++ app/Dummy.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <span>{{test}}</span>
+  </div>
+</template>
+<style scope>
+</style>
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component'
+
+let a = 1
+a = "string"
+
+@Component()
+export default class App extends Vue {
+  test: "abc"
+}
+</script>
